0a1a949530c2e7cfd313296357321ebc03decf34
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2023-12-20 18:38:00 +0530

    url changed base

--- src/Redux/apislice.js
+++ src/Redux/apislice.js
@@ -1,7 +1,7 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 
-const baseQuery=fetchBaseQuery({baseUrl:'http://www.shop-electron.shop'})
+const baseQuery=fetchBaseQuery({baseUrl:'http://www.shop-electron.shop/api/'})
 
 // const baseQuery=fetchBaseQuery({baseUrls:"http://127.0.0.1:8000"})
 
