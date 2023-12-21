af9840dc556ce4f162d5b2e581beb39258966914
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2023-12-21 16:02:23 +0530

    premium mem

--- src/Components/User/Profile.jsx
+++ src/Components/User/Profile.jsx
@@ -272,7 +272,7 @@ const Profile = () => {
                       </CardBody>
                       <Divider />
                       <CardFooter>
-                        {userprofile.premium_member?(<Button radius="full"
+                        {userprofile.premium_member == "true"?(<Button radius="full"
                           className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">Premium Member</Button>):(<Button
                           onClick={() => handlePaymentButtonClick(items.id,items.price)}
                           radius="full"
