da2b1441603f16f7df4ca8b36b62438f49e225b0
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2023-12-21 16:26:41 +0530

    premsss

--- src/Components/User/Profile.jsx
+++ src/Components/User/Profile.jsx
@@ -223,7 +223,7 @@ const Profile = () => {
             <p className="text-zinc-500">500</p>
           </div>
         </div>
-        {userprofile.premium_member === "true" ? (
+        {userprofile.premium_member ? (
           <button className="btn btn-wide mt-3 ml-8 bg-cyan-700 btn-circle text-cyan-500">
             Premium Member
           </button>
